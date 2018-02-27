dad31a98ca2c6cf9b6a1c554bb263ca080d26f8f
Bram Ceulemans <bramceulemans@me.com>
2018-02-28 00:37:54 +0100

    Removed docs

--- src/main/java/net/minecraft/api/controller/StatisticController.java
+++ src/main/java/net/minecraft/api/controller/StatisticController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.ApplicationScope;
 
-@ApplicationScope
+@CrossOrigin
 @RestController("minecraft-api-controller")
 @RequestMapping(path = "/api")
 public class StatisticController {
